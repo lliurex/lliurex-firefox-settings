32ef8cb38a603969fef94735b47c84f4a541504d
juanma1980 <juanma1980@gmail.com>
2018-10-03 17:19:31 +0200

    Bionic release

---
+++ lliurex-firefox-settings.install/usr/lib/firefox/browser/defaults/preferences/lliurex-defaults.js
@@ -0,0 +1,34 @@
+// Place your global Firefox preferences in this file if you are using
+// ubufox. Especially those preferences defined in ubufox need to be configured
+// here to become effective.
+
+// Example: Homepage
+pref("browser.startup.homepage", "file:/etc/firefox/lliurex-firefox.properties");
+pref("browser.startup.restore_default_bookmarks", true);
+pref('general.config.obscure_value',0);
+pref('general.config.filename','lliurex.cfg');
+//defaultPref("browser.startup.homepage","data:text/plain,browser.startup.homepage=http://server");
+// // Disable offline/disk caching of web pages - good if you have a central proxy
+// pref("browser.cache.offline.enable", false);
+// pref("browser.cache.disk.enable", false);
+//
+// // Disable session restore - useful when users log out without closing
+// Firefox first - saves server
+// // CPU, memory and bandwidth when they restart Firefox and open up
+// lots of tabs from previous session
+// pref("browser.sessionrestore.enabled", false);
+//
+// // Open search results in new tab
+// pref("browser.search.openintab", true);
+//
+// // Reduce number of bookmark backups from 10 to 5
+// pref("browser.bookmarks.max_backups", 5);
+//
+// // Reduce number of tabs you an undo after closing from 10 to 3
+// pref("browser.sessionstore.max_tabs_undo", 3);
+//
+// //Reduce maximum number of simultaneous popups from 20 to 3
+// pref("dom.popup_maximum", 3);
+//
+// // Increased number of connections from 15 to 24
+// pref("network.http.max-connections", 24); 
