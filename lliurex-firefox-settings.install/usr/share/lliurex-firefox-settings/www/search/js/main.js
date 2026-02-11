43acb1c79e307f284c75f353afba70a5633a1cb6
jrpelegrina <juapesai@hotmail.com>
2026-02-11 10:24:32 +0100

    Fixed

--- lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/search/js/main.js
+++ lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/search/js/main.js
@@ -1,3 +1,38 @@
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
+
+function rss(){
+    list_rss = ["https://portal.edu.gva.es/cvtic/va/category/inici/feed/","https://portal.edu.gva.es/lliurex/?feed=rss2"];
+    for( x in list_rss){
+        console.log(x);
+        fetch("https://cors-anywhere.herokuapp.com/"+list_rss[x])
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            console.log(data);
+            const items = data.querySelectorAll("item");
+            let html = document.querySelector("#rss").innerHTML;
+            items.forEach(el => {
+                let stringscaped = escapeHtml(el.querySelector("description").childNodes[0].nodeValue);
+            html += `
+                <div class="row">
+                    <a href="${el.querySelector("link").innerHTML}" title="${stringscaped}">${el.querySelector("title").innerHTML}</a>
+    
+                </div>
+            `;
+            document.querySelector("#rss").innerHTML = html;
+            });
+        });
+    }
+}
+rss();
+
 async function checkServer(url,elementId){
 
     try{
@@ -7,4 +42,4 @@ async function checkServer(url,elementId){
          console.log(`URL ${url} not accesible`)   
     }
 }
-checkServer('http://server','conditional-site')
+checkServer('http://server','conditional-bookmark')
