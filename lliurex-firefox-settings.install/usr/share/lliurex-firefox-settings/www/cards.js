e06cf1a75af6bc38df4d249bd174cd22b38fee97
M.Angel Juan <m.angel.juan@gmail.com>
2026-08-11 13:12:06 +0200

    Use cards.js for allow load code

---
+++ lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/cards.js
@@ -0,0 +1,16 @@
+window.LLX_CARDS = {
+    "cards": [
+        {
+            "title": "Easy-Sites",
+            "url": "http://server/easy-sites",
+            "image": "images/easy-sites.svg",
+            "condition": "http://server/easy-sites"
+        },
+        {
+            "url": "https://www.linux.com"
+        },
+        {
+            "url": "http://server:8000"
+        }
+    ]
+};
