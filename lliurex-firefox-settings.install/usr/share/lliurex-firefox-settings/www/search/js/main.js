2b1e07c6226dbd957a8917494699f2cbf4ada464
M.Angel Juan <m.angel.juan@gmail.com>
2026-08-11 12:31:17 +0200

    remove malformed lines

--- lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/search/js/main.js
+++ lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/search/js/main.js
@@ -1,43 +1,4 @@
-<<<<<<< Updated upstream
-function escapeHtml(unsafe) {
-    return unsafe
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;");
- }
-
-function rss(){
-    list_rss = ["https://portal.edu.gva.es/cvtic/va/category/inici/feed/","https://portal.edu.gva.es/lliurex/?feed=rss2"];
-    for( x in list_rss){
-        console.log(x);
-        fetch("https://cors-anywhere.herokuapp.com/"+list_rss[x])
-        .then(response => response.text())
-        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-        .then(data => {
-            console.log(data);
-            const items = data.querySelectorAll("item");
-            let html = document.querySelector("#rss").innerHTML;
-            items.forEach(el => {
-                let stringscaped = escapeHtml(el.querySelector("description").childNodes[0].nodeValue);
-            html += `
-                <div class="row">
-                    <a href="${el.querySelector("link").innerHTML}" title="${stringscaped}">${el.querySelector("title").innerHTML}</a>
-    
-                </div>
-            `;
-            document.querySelector("#rss").innerHTML = html;
-            });
-        });
-    }
-}
-rss();
-
-async function checkServer(url,elementId){
-=======
 const DEFAULT_IMAGE = 'images/default.png';
->>>>>>> Stashed changes
 
 function isValidUrl(url) {
     if (typeof url !== 'string') {
@@ -55,9 +16,6 @@ function isValidUrl(url) {
         return false;
     }
 }
-<<<<<<< Updated upstream
-checkServer('http://server','conditional-bookmark')
-=======
 
 async function isReachable(url) {
     try {
@@ -116,4 +74,3 @@ async function loadCards() {
 }
 
 loadCards();
->>>>>>> Stashed changes
