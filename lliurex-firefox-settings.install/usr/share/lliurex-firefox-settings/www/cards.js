3e5b541e88779d1cd95c92e2efa48882c3f135aa
M.Angel Juan <m.angel.juan@gmail.com>
2026-08-13 12:39:50 +0200

    remove test code

--- lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/cards.js
+++ lliurex-firefox-settings.install/usr/share/lliurex-firefox-settings/www/cards.js
@@ -5,12 +5,6 @@ window.LLX_CARDS = {
             "url": "http://server/easy-sites",
             "image": "images/easy-sites.png",
             "condition": "http://server/easy-sites"
-        },
-        {
-            "url": "https://www.google.com"
-        },
-        {
-            "url": "http://server:8000"
         }
     ]
 };
